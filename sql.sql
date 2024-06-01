4654e163e8bbdbcacb66ce7e4514c41a38a58265
MishalTariq2005 <149159057+MishalTariq2005@users.noreply.github.com>
2024-06-01 14:45:33 +0500

    Add files via upload

---
+++ sql.sql
@@ -0,0 +1,6 @@
+create database bill_system;
+use bill_system;
+
+create table Register(meter_no varchar(20),name varchar(30),email varchar(50), password varchar(30), phoneno varchar(11));
+select * from Register;
+select * from Register;
